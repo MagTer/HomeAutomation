2cf16cafb8dd392301d3e0832cb204a9e8972174
Magnus Ternström <magnus@falle.se>
2022-02-14 21:11:34 +0100

    Changed Authlevel

--- AcControl/GetAcTarget.cs
+++ AcControl/GetAcTarget.cs
@@ -17,7 +17,7 @@ namespace AcControl
         }
 
         [Function("GetAcTarget")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
             TemperatureObject myTemp = new TemperatureObject(req);
             _logger.LogInformation("Inputs - inside:  " + myTemp.IndoorTemp + " - outside: " + myTemp.OutdoorTemp + " - target: " + myTemp.RoomTarget);
