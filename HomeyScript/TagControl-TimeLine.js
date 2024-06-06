6a0f67b6cfe3da82e0c57e0de566ac4a131a9a2f
Magnus Ternström <magnus@falle.se>
2024-06-06 12:48:02 +0200

    Update TagControl-TimeLine.js

--- HomeyScript/TagControl-TimeLine.js
+++ HomeyScript/TagControl-TimeLine.js
@@ -1,9 +1,11 @@
 // This Homeyscript is intended to be run every minute to get frequent updates on the tags but it does not do anything if there are no new log lines in the timeline.
+// The config section includes parameters for line count and line length to adjust for different display sizes on the tags
 
 
 // Config
 var tagConfigs = [
-  { tagDeviceName: "M2 Tag 6" }
+  { tagDeviceName: "M2 Tag 6", lineCount: 5, lineLengthCap: 34},
+  { tagDeviceName: "M2 4 - Tag 2", lineCount: 13, lineLengthCap: 50}
 ];
 
 
@@ -16,18 +18,36 @@ function GetDeviceID(currentDeviceName) {
   }
 }
 
-async function GetFilteredNotifications() {
+async function GetNotifications() {
   const response = await Homey.notifications.getNotifications();
   const items = Object.values(response);
-
   items.sort((a, b) => new Date(b.dateCreated) - new Date(a.dateCreated));
+  return items;
+}
 
-  const lastItems = items.slice(0, 5);
+
+async function GetNotificationUpdateNeed(items) {
+  var localNeedsUpdate = false
+  const mostRecentTimestamp = new Date(items[0].dateCreated);
+  mostRecentTimestamp.setHours(mostRecentTimestamp.getHours() + 2);
+  const currentTime = new Date();
+  currentTime.setHours(currentTime.getHours() + 2);
+  const timeDifference = currentTime.getTime() - mostRecentTimestamp.getTime();
+  if (timeDifference < 100000) {localNeedsUpdate = true}; 
+  log("Time diff: " + timeDifference)  
+  log("Update check return value: " + localNeedsUpdate)
+  return localNeedsUpdate;
+}
+
+
+
+async function GetFilteredNotifications(items, lineCount, lineLengthCap) {
+
+  const lastItems = items.slice(0, lineCount);
 
   
   const excerptsWithTimestamp = lastItems.map(item => {
     const timestamp = new Date(item.dateCreated);
-  
     timestamp.setHours(timestamp.getHours() + 2);
     const hours = timestamp.getHours().toString().padStart(2, '0');
     const minutes = timestamp.getMinutes().toString().padStart(2, '0');
@@ -39,20 +59,17 @@ async function GetFilteredNotifications() {
     activeExerpt = activeExerpt.replace("har automatiskt uppdaterats till version","har uppdaterats:");
     activeExerpt = activeExerpt.replace(" Ternström","");
     activeExerpt = activeExerpt.replace(" Kastemyr","");
-    if (activeExerpt.match("Nivå")) { activeExerpt = activeExerpt.substr(0, activeExerpt.indexOf(" Nivå")); }
-    activeExerpt = activeExerpt.length > 32 ? activeExerpt.substring(0, 33) : activeExerpt;
 
+    // Trim based on specific words if the current screen is small
+    if (lineLengthCap < 42) {
+       if (activeExerpt.match("Nivå:")) { activeExerpt = activeExerpt.substr(0, activeExerpt.indexOf(" Nivå")); }
+       if (activeExerpt.match("Temp:")) { activeExerpt = activeExerpt.substr(0, activeExerpt.indexOf(" Temp")); }
+    }
+
+    activeExerpt = activeExerpt.length > lineLengthCap ? activeExerpt.substring(0, lineLengthCap + 1) : activeExerpt;
     return `${timestampStr} ${activeExerpt}`;
   });
 
-  // Check if the recent log line is created within the last 100 seconds
-  const mostRecentTimestamp = new Date(lastItems[0].dateCreated);
-  mostRecentTimestamp.setHours(mostRecentTimestamp.getHours() + 2);
-  const currentTime = new Date();
-  currentTime.setHours(currentTime.getHours() + 2);
-  const timeDifference = currentTime.getTime() - mostRecentTimestamp.getTime();
-  if (timeDifference < 100000) {needsUpdate = true}; 
-  log("Time diff: " + timeDifference)  
   return excerptsWithTimestamp;
 }
 
@@ -60,14 +77,15 @@ function CreateTagContent(logLines) {
   log('Generating tag content')
   //log(logLines)
   var responseJSON = "["
-  responseJSON +="{ \"text\": [2, 2, \"Automationslogg\", \"fonts/bahnschrift20\", 1, 0, 120 ] },"
-  responseJSON +="{ \"line\": [2,21,294,21,2]},"
-  responseJSON +="{ \"line\": [2,22,294,22,2]},"
-  responseJSON +="{ \"text\": [2, 30, \"" + logLines[0] + "\", \"fonts/calibrib16\", 1, 0, 0 ] },"
-  responseJSON +="{ \"text\": [2, 50, \"" + logLines[1] + "\", \"fonts/calibrib16\", 1, 0, 0 ] },"
-  responseJSON +="{ \"text\": [2, 70, \"" + logLines[2] + "\", \"fonts/calibrib16\", 1, 0, 0 ] },"
-  responseJSON +="{ \"text\": [2, 90, \"" + logLines[3] + "\", \"fonts/calibrib16\", 1, 0, 0 ] },"
-  responseJSON +="{ \"text\": [2, 110, \"" + logLines[4] + "\", \"fonts/calibrib16\", 1, 0, 0 ] }"
+  responseJSON +="{ \"text\": [4, 3, \"Automationslogg\", \"fonts/bahnschrift20\", 1, 0, 120 ] },"
+  responseJSON +="{ \"line\": [4,21,400,21,2]},"
+  responseJSON +="{ \"line\": [4,22,400,22,2]},"
+
+  for (let l = 0; l < logLines.length; l++) {
+    var yAxis = 30 + (l * 20)
+    responseJSON +="{ \"text\": [4, " + yAxis + ", \"" + logLines[l] + "\", \"fonts/calibrib16\", 1, 0, 0 ] },"
+  }
+
   responseJSON +="]"
   log("JSON: " + responseJSON)
   return responseJSON;
@@ -90,18 +108,16 @@ async function UpdateTag(currentDeviceName, tagContent) {
 
 
 // Script execution
-log('Starting tag updates');
-var needsUpdate = false;
+log('Starting tag update script');
+const homeyNotifications = await GetNotifications();
+const needsUpdate = await GetNotificationUpdateNeed(homeyNotifications);
 const devices = await Homey.devices.getDevices();
-const logLines = await GetFilteredNotifications();
-var tagContent = CreateTagContent(logLines);
-// Debug
-// needsUpdate = true
-
 
 if (needsUpdate) {
   for (let i = 0; i < tagConfigs.length; i++) {
     log('Updating tag ' + tagConfigs[i].tagDeviceName);
+    var logLines = await GetFilteredNotifications(homeyNotifications, tagConfigs[i].lineCount, tagConfigs[i].lineLengthCap);
+    var tagContent = CreateTagContent(logLines);
     await UpdateTag(tagConfigs[i].tagDeviceName, tagContent);
     await wait(200);
     log(' ');
